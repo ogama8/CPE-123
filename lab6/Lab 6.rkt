62059dc260802941c1492747dc455ee09c88ba4b
Eli Backer <ogama8@gmail.com>
2011-10-06 22:34:39 -0700

    lab6 commit

--- lab6/Lab 6.rkt
+++ lab6/Lab 6.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |Lab 6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#lang racket
+
 ;; Exercise 20
 (define (string-join str1 str2) 
   (string-append str1 "_" str2))
@@ -17,3 +16,42 @@
 
 (string-insert "goodday" 4)
 
+
+;; 4 Sound
+(require (planet "main.rkt" ("clements" "rsound.plt" 2 5)))
+(define (ms x) (* 44.1 x))
+
+;; Stutter
+(define (eight snd) (clip snd 0 (round (* (rsound-frames snd) 1/8))))
+(define (stutter rs) (play (rs-append* (for/list ([i (in-range 8)]) (eight rs)))))
+
+;;(stutter (rs-read/clip "/Users/Eli/Documents/CP Fall 2011/CPE-123/Assignment1/hg.wav" 0 441000))
+
+
+;; Chord
+;; (make-tone pitch(hz) vol(0-1) frames)
+(define (chord x y z) (overlay (make-tone x 1/4 (ms 1000)) 
+                               (overlay (make-tone y 1/4 (ms 1000)) 
+                                        (make-tone z 1/4 (ms 1000)))))
+
+;;(play (chord 440 (* 440 3/2) (* 440 5/4)))
+
+;; Classify
+(define (classify x y) (cond [(or (= (/ x y) (/ 3 2)) (= (/ x y) (/ 2 3))) "fifth"]
+                             [(or (or (= (/ x y) (/ 5 4)) (= (/ x y) (/ 4 5))) (or (= (/ x y) (/ 6 5)) (= (/ x y) (/ 5 6)))) "third"]
+                             [else "unknown"]))
+
+
+;; Noisy
+(define (noisy n) (/ (- (random 100) 50) 200))
+
+;;(play (mono-signal->rsound 44100 noisy))
+
+
+
+;; Squarewave
+(define(squarewave n) (cond [(= (/ (truncate (/ n 100)) 2) 
+                                (truncate (/ (truncate (/ n 100)) 2))) 10000]
+                            [else 0]))
+
+;;(play (mono-signal->rsound 44100 squarewave))
