37414afa2d832142be1fb1da0dc8d99fd89a1132
Eli Backer <ogama8@gmail.com>
2011-10-12 16:51:07 -0700

    lab7 final

--- lab7/lab7.rkt
+++ lab7/lab7.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname lab7) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;;#lang racket
 (require (planet "main.rkt" ("clements" "rsound.plt" 2 5)))
 (define (ms x) (* 44.1 x))
 
@@ -63,3 +64,11 @@
 
 
 
+;;  Name to Rsound
+;; 440 * 2^((n-69)/12)
+;;(check-expect (name->rsound "a" 4) 440)
+(define (name->rsound nn oct) 
+  (local ((define (tone f) (sin (* f 2 pi 1/44100 (* 440 (expt 2 (/ (- (name->note-num nn oct) 69) 12)))))))
+  (mono-signal->rsound 22050 tone)))
+
+(play (name->rsound "a" 4))
